9c1967bf16d099dadc40858f06fe9b0061f632d3
Zhaokai Guan <zguan25@myseneca.ca>
2024-02-27 12:35:20 -0500

    Main page set up

--- my-resume/app/page.tsx
+++ my-resume/app/page.tsx
@@ -1,10 +1,20 @@
-import { WavyBackground } from "@/components/wavy-background";
-import Image from "next/image";
+"use client";
+import { SparklesPreview } from "@/components/assets/SparklesAsset";
+import React, { useState, useEffect } from "react";
+import dynamic from "next/dynamic";
+
+import "./page.css";
 
 export default function Home() {
   return (
-    <main >
-      <WavyBackground></WavyBackground>
+    <main>
+      <div>
+        <SparklesPreview />
+      </div>
+      <div>
+        {/* Your new content goes here */}
+        <h1>Welcome to the new content!</h1>
+      </div>
     </main>
   );
 }
---
+++ my-resume/components/assets/SparklesAsset.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { SparklesCore } from "../aceternity-ui-components/sparkles";
+
+export function SparklesPreview() {
+  return (
+    <div className="h-[40rem] relative w-full bg-black flex flex-col items-center justify-center overflow-hidden rounded-md">
+      <div className="w-full absolute inset-0 h-screen">
+        <SparklesCore
+          id="tsparticlesfullpage"
+          background="transparent"
+          minSize={0.6}
+          maxSize={1.4}
+          particleDensity={100}
+          className="w-full h-full"
+          particleColor="#FFFFFF"
+        />
+      </div>
+      <h3 className="md:text-7xl text-3xl lg:text-3xl text-center text-white relative z-10 mb-20">
+        There are 2 trillion planets in this universe,
+      </h3>
+      <h3 className="md:text-7xl text-3xl lg:text-3xl text-center text-white relative z-10 mb-20">
+        and 8.1 billion human beings on this planet.
+      </h3>
+      <h3 className="md:text-7xl text-3xl lg:text-3xl text-center text-white relative z-10 mb-20">
+        Hello and nice to meet you,stranger. My name is.
+      </h3>
+      <h1 className="md:text-7xl text-3xl lg:text-6xl font-bold text-center text-white relative z-20 mb-10">
+        Roger Guan 
+      </h1>
+    </div>
+  );
+}
