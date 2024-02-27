910027c50d0dbc538fa9e8e12c4e89c5abcc1555
Zhaokai Guan <zguan25@myseneca.ca>
2024-02-27 11:21:52 -0500

    adding wavy effects from the aceternity UI

---
+++ my-resume/app/page.tsx
@@ -0,0 +1,10 @@
+import { WavyBackground } from "@/components/wavy-background";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main >
+      <WavyBackground></WavyBackground>
+    </main>
+  );
+}
